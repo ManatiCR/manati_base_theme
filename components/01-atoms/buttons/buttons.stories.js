698da777c3d3ae34917f7f83124d2f392b536647
Sergio Benavides <sbenavides@estudiomanati.com>
2020-09-23 08:26:14 -0600

    clean button component

--- components/01-atoms/buttons/buttons.stories.js
+++ components/01-atoms/buttons/buttons.stories.js
@@ -3,11 +3,6 @@ import React from 'react';
 import button from './button.twig';
 
 import buttonData from './button.yml';
-import buttonAltData from './button-alt.yml';
-import buttonAltDataRed from './button-alt-red.yml';
-import buttonOutlineData from './button-outline.yml';
-import buttonDownloadData from './button-download.yml';
-import buttonLinkData from './button-link.yml';
 
 /**
  * Storybook Definition.
@@ -16,24 +11,4 @@ export default { title: 'Atoms/Button' };
 
 export const example = () => (
   <div dangerouslySetInnerHTML={{ __html: button(buttonData) }} />
-);
-
-export const exampleAlt = () => (
-  <div dangerouslySetInnerHTML={{ __html: button(buttonAltData) }} />
-);
-
-export const exampleAltRed = () => (
-  <div dangerouslySetInnerHTML={{ __html: button(buttonAltDataRed) }} />
-);
-
-export const exampleOutline = () => (
-  <div dangerouslySetInnerHTML={{ __html: button(buttonOutlineData) }} />
-);
-
-export const exampleDownload = () => (
-  <div dangerouslySetInnerHTML={{ __html: button(buttonDownloadData) }} />
-);
-
-export const exampleLink = () => (
-  <div dangerouslySetInnerHTML={{ __html: button(buttonLinkData) }} />
 );
